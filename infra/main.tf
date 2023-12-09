e86a3a37285e3071d386d8bf7fe2a675d9fdd647
flavono123 <flavono123@gmail.com>
2023-12-09 16:41:46 +0900

    (WIP)feat: 2.1.4 주키퍼, 카프카 브로커 실행
    - 카프카 2.5.0(스칼라 2.12) 다운 받고 카프카 브로커와 주키퍼 실행
    - 자바 힙 메모리 400M로 구성
    - 카프카 server.properties의 advertised.listeners만 EC2 퍼블릭 아이피로 노출
    - 보안 그룹 인바운드 규칙 9092, 2181 **모든 IP에 대해서** 허용
    - 로컬에 같은 버전 카프카를 설치하고 클라이언트 사용(TODO: local-exec provisioner로 설치)
    - 문제점
      - EC2 User data가 실행 중 중단된다 -> remote-exec provisioner에서 카프카 설치, 실행
      - remote-exec provisioner inline으로도 브로커 실행이 안된다(주키퍼는 됨)
      - bin/kafka-broker-api-versions.sh(와 다른 클라이언트도) 실행 시 보안 그룹 인바운드
        **모든 IP에 대해서** 허용이 아니라면 실행이 안된다

--- infra/main.tf
+++ infra/main.tf
@@ -42,7 +42,8 @@ module "instance" {
 
   user_data = <<EOT
     #!/bin/bash
-    sudo yum install -y java-1.8.0-openjdk
+    # Install Java
+    sudo yum install -y java-1.8.0-openjdk-devel
     java -version
   EOT
 }
@@ -59,9 +60,22 @@ module "instance_sg" {
   name        = local.name
   description = "Security group for ${local.name} practice"
 
-  # TODO: 브로커(9092)와 주키퍼(2181)
+  # HACK: 브로커(9092)와 주키퍼(2181) source cidr all이어야 동작함. 왜?
   ingress_cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
-  ingress_rules       = ["ssh-tcp"]
+  ingress_rules = [
+    "ssh-tcp",
+  ]
+  ingress_with_cidr_blocks = [
+    {
+      rule        = "kafka-broker-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      rule        = "zookeeper-2181-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
 
   egress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules       = ["all-all"]
@@ -87,5 +101,32 @@ resource "terraform_data" "private_key_pem" {
       echo '${module.instance_key.private_key_openssh}' > ${local.name}.pem
       chmod 400 ${local.name}.pem
     EOT
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("${local.name}.pem")
+    host        = module.instance.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      # Download Kafka
+      "wget https://archive.apache.org/dist/kafka/2.5.0/kafka_2.12-2.5.0.tgz",
+      "tar -xzf kafka_2.12-2.5.0.tgz",
+      "cd kafka_2.12-2.5.0",
+      "export KAFKA_HEAP_OPTS='-Xmx400M -Xms400M'",
+      "echo \"export KAFKA_HEAP_OPTS='-Xmx400M -Xms400M'\" >> ~/.bashrc",
+      # Start Zookeeper
+      "bin/zookeeper-server-start.sh -daemon config/zookeeper.properties",
+      # Set Kafka config
+      "export PUBLIC_IP=$(curl -q http://checkip.amazonaws.com/)",
+      "sed -i \"s%#advertised.listeners=PLAINTEXT://your.host.name:9092%advertised.listeners=PLAINTEXT://$PUBLIC_IP:9092%g\" config/server.properties",
+      # Start Kafka
+      "bin/kafka-server-start.sh -daemon config/server.properties",
+      # HACK: last line seems to be ignored, so add a dummy line; X -> there should be another reason. this line excuted every time.
+      "echo 'done'",
+    ]
   }
 }
