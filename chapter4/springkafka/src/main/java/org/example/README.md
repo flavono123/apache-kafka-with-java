fe2fc5e389344acec00ea30f847c38f6c46678a9
flavono123 <flavono123@gmail.com>
2024-01-11 16:12:20 +0900

    feat: spring kafka producer; custom template

---
+++ chapter4/springkafka/src/main/java/org/example/README.md
@@ -0,0 +1,21 @@
+# 스프링 카프카
+
+## 기본 카프카 템플릿
+
+```yaml
+# application.yaml
+spring.kafka.producer.acks
+spring.kafka.producer.batch-size
+spring.kafka.producer.bootstrap-servers
+spring.kafka.producer.buffer-memory
+spring.kafka.producer.client-id
+spring.kafka.producer.compression-type
+spring.kafka.producer.key-serializer
+spring.kafka.producer.properties.*
+spring.kafka.producer.retries
+spring.kafka.producer.transaction-id-prefix
+spring.kafka.producer.value-serializer
+```
+
+## 커스텀 카프카 템플릿
+- send 안됨
--- chapter4/springkafka/src/main/java/org/example/SpringProducerApplication.java
+++ chapter4/springkafka/src/main/java/org/example/SpringProducerApplication.java
@@ -4,13 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaProducerException;
+import org.springframework.kafka.core.KafkaSendCallback;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 
 @SpringBootApplication
 public class SpringProducerApplication implements CommandLineRunner {
     private static final String TOPIC_NAME = "test";
     @Autowired
-    private KafkaTemplate<Integer, String> template;
+    private KafkaTemplate<String, String> customKafkaTemplate;
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(SpringProducerApplication.class);
@@ -19,9 +23,16 @@ public class SpringProducerApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        for (int i = 0; i < 10; i++) {
-            template.send(TOPIC_NAME, "test" + i);
-        }
+        ListenableFuture<SendResult<String, String>> future = customKafkaTemplate.send(TOPIC_NAME, "custom-test");
+        future.addCallback(new KafkaSendCallback<String, String>() {
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+            }
+
+            @Override
+            public void onFailure(KafkaProducerException ex) {
+            }
+        });
 
         System.exit(0);
     }
