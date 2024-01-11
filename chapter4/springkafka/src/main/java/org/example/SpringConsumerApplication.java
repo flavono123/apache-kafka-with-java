14098dc5495781e3cb0329d1589c13ebad98dd81
flavono123 <flavono123@gmail.com>
2024-01-11 16:58:53 +0900

    feat: spring kafka consumer; ack and consumeraware

--- chapter4/springkafka/src/main/java/org/example/SpringConsumerApplication.java
+++ chapter4/springkafka/src/main/java/org/example/SpringConsumerApplication.java
@@ -1,6 +1,7 @@
 package org.example;
 
 
+import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.PartitionOffset;
 import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.support.Acknowledgment;
 
 import java.util.List;
 
@@ -22,24 +24,11 @@ public class SpringConsumerApplication {
     }
 
     @KafkaListener(topics = "test", groupId = "test-record-listener")
-    public void recordListener(ConsumerRecords<String, String> records) {
+    public void recordListener(ConsumerRecords<String, String> records, Acknowledgment ack) {
         records.forEach(r -> logger.info(r.toString()));
+        ack.acknowledge();
     }
 
-    @KafkaListener(topics = "test", groupId = "test-string-listener")
-    public void stringListener(List<String> messages) {
-        messages.forEach(m -> logger.info(m));
-    }
-
-    @KafkaListener(topics = "test", groupId = "test-property-listener", properties = {"max.poll.interval.ms:60000", "auto.offset.reset:earliest"})
-    public void propertyListener(List<String> messages) {
-        messages.forEach(m -> logger.info(m));
-    }
-
-    @KafkaListener(topics = "test", groupId = "test-concurrent-listener", concurrency = "3")
-    public void concurrentListener(List<String> messages) {
-        messages.forEach(m -> logger.info(m));
-    }
 
     @KafkaListener(topicPartitions = {
             @TopicPartition(topic = "test01", partitions = {"0", "1"}),
@@ -47,7 +36,8 @@ public class SpringConsumerApplication {
         },
         groupId = "test-partition-listener"
     )
-    public void partitionListener(ConsumerRecords<String, String> records) {
+    public void partitionListener(ConsumerRecords<String, String> records, Consumer<String, String> consumer) {
         records.forEach(r -> logger.info(r.toString()));
+        consumer.commitAsync();
     }
 }
